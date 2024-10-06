be8ee14fa0c139695b1e27166b0e42f046cbde5f
nutjatuwit <103878796+nutjatuwit0808@users.noreply.github.com>
2024-10-06 22:51:17 +0700

    update lesson

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -1,24 +1,58 @@
 import React from "react";
-import { getAvailableNewsYears, getNewsForYear } from "../../../../lib/news";
+import {
+  getAvailableNewsYears,
+  getNewsForYear,
+  getAvailableNewsMonths,
+  getNewsForYearAndMonth,
+} from "../../../../lib/news";
 import Link from "next/link";
+import NewsList from "../../../../components/news-list";
 
-export default function FilteredNewsPage({params}) {
+export default function FilteredNewsPage({ params }) {
   const filter = params.filter;
-  console.log(filter)
-  const links = getAvailableNewsYears();
+  let selectedYear = filter?.[0];
+  let selectedMonth = filter?.[1];
+
+  let news;
+  let links = getAvailableNewsYears();
+
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
+  }
+
+  if(selectedYear && selectedMonth) {
+    news  = getNewsForYearAndMonth(selectedYear, selectedMonth)
+    links = []
+  }
+
+  let newsContent = <p>No news found for the selected period.</p>;
+
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />;
+  }
+
+  console.log(filter);
+
   return (
-    <header id="archive-header">
-      <nav>
-        <ul>
-          {links.map((link) => {
-            return (
-              <li key={link}>
-                <Link href={`/archive/${link}`}>{link}</Link>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : ` /archive/${link}`;
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
   );
 }
